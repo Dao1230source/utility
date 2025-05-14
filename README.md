ce646e0d02527ddf45342cfbfd3a48c59521f4ef
Dao1230 <dao1230.source@gmail.com>
2025-05-15 02:20:46 +0800

    Enums

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 ### 本项目发布一些有意思的工具类
+
 #### Assign 批量赋值
-开发过程中，我们经常遇到这样的情况：需要通过集合中对象的某个Key值批量区查询其他的数据再给其他字段赋值。最常见的是通过用户ID查询用户名称。  
+
+开发过程中，我们经常遇到这样的情况：需要通过集合中对象的某个Key值批量去查询其他的数据再给其他字段赋值。最常见的是通过用户ID查询用户名称。  
 一般来说我们是这样做的:
+
 ```java
 // 以下为伪代码
 public void assign() {
     // 假设这是一个班级的所有学生，其中只有id，需要给name赋值
-    Collecttion<Student> studentsOfClass;
+    Collecttion<StudentInfo> studentsOfClass;
     // 获取students中的studentId
     Set<String> studentIds;
     // 从学生表获取学生信息
@@ -22,14 +25,43 @@ public void assign() {
     }
 }
 ```
-这样简单易实现，可是存在以下一些不完美的地方：
-- 项目中存在大量相似的重复代码，且存在不少隐藏bug，比如toMap时因为id重复而报错
-- 有时可能不止给一个字段赋值，需要简单重复以上过程，代码写起来十分不简洁
-- 如果存在多个赋值过程，导致请求时间过长，影响接口性能，需要使用多线程处理，但是每个人的处理方法不同，代码更是五花八门，而且线程相关的隐藏bug更多
-- 如果集合数量过多，需要分批处理
-- 如果其中一个赋值过程发生了异常，应该如何处理？
-- 如果某些数据需要用到缓存来提高性能该如何实现？
-- 一些赋值有依赖关系，比如先通过studentId获取teacherId，再通过teacherId获取teacherName
-- 集合数据需要分类处理，比如高中生需要查询高考时间，初中生需要查询中考时间，小学生则无须处理
 
-以上种种问题，[Assign](Assign.md)均可完美实现
+使用Assign组件只要如下编写代码即可实现
+
+```java
+public void assign() {
+    Collecttion<StudentInfo> studentsOfClass;
+    Assign.build(studentsOfClass)
+            // 从学生表获取学生信息，并以studentId为唯一Key
+            .addAcquire(this::fetchStudentInfoFromStudentTable, Student::getId)
+            // 根据获取StudentInfo中的studentId去获取学生信息
+            .addAction(StudentInfo::getId)
+            // 赋值
+            .addAssemble(Student.getName, StudentInfo::setName)
+            // 最后执行
+            .backAcquire().backAssign().invoke();
+}
+```
+## Assign具有以下优势
+
+### 标准化流程
+    规范取值赋值的标准写法，提高开发效率
+### 践行代码简洁之道
+    程序员只需关注真正业务相关的事物，无须理会其他细节
+### 规避常见易错bug
+    比如toMap时因为id重复而报错
+### 多线程并行
+    提高性能，避免使用线程导致的各种bug
+### 分批处理
+    避免接口输入输出过大引发的各种内存、超时等问题
+### 异常处理机制
+    可以自行处理一个赋值过程的异常
+    指定中断策略：全部成功/部分成功/全部失败，可继续流转至下一个节点
+### 数据缓存
+    使用 caffeine 缓存获取的数据
+### 节点依赖
+    一些赋值有依赖关系，比如先通过studentId获取teacherId，再通过teacherId获取teacherName
+### 数据分类处理
+    比如高中生需要查询高考时间，初中生需要查询中考时间，小学生则无须处理
+
+更多详情请见：[Assign](Assign.md)
--- src/main/java/org/source/utility/utils/Enums.java
+++ src/main/java/org/source/utility/utils/Enums.java
@@ -1,5 +1,6 @@
 package org.source.utility.utils;
 
+import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.source.utility.constant.Constants;
 import org.source.utility.function.SFunction;
@@ -13,43 +14,71 @@ import java.util.stream.Collectors;
 /**
  * @author zengfugen
  */
+@UtilityClass
 @Slf4j
 public class Enums {
-    private Enums() {
-        throw new IllegalStateException("Utility class");
-    }
 
     private static final Map<String, Map<Object, Enum<?>>> ENUM_MAP = new ConcurrentHashMap<>(32);
 
-    @SuppressWarnings("unchecked")
-    public static <E extends Enum<E>, K> E getEnum(Class<E> enumClass,
-                                                   SFunction<E, K> keyGetter,
-                                                   K k) {
-        if (Objects.isNull(k) || Objects.isNull(enumClass) || Objects.isNull(keyGetter)) {
-            return null;
+    public static <E extends Enum<E>, K> Map<Object, Enum<?>> enumMapByKey(Class<E> enumClass, SFunction<E, K> keyGetter) {
+        if (Objects.isNull(enumClass) || Objects.isNull(keyGetter)) {
+            return Map.of();
         }
         if (!enumClass.isEnum()) {
-            throw new IllegalArgumentException("class不是一个枚举类: " + enumClass.getName());
+            log.warn("class不是一个枚举类: {}", enumClass.getName());
+            return Map.of();
         }
         String enumName = enumClass.getSimpleName();
         String fieldName = "";
         try {
             fieldName = Lambdas.getFieldName(keyGetter);
         } catch (Exception e) {
-            log.debug("get field name error", e);
+            log.warn("get field name error", e);
         }
         String key = String.join(Constants.UNDERSCORE, enumName, fieldName);
-        ENUM_MAP.computeIfAbsent(key, v -> {
+        return ENUM_MAP.computeIfAbsent(key, v -> {
             E[] enums = enumClass.getEnumConstants();
             return Arrays.stream(enums).collect(Collectors.toConcurrentMap(keyGetter, e -> e));
         });
-        return (E) ENUM_MAP.get(key).get(k);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <E extends Enum<E>, K> Map<K, E> genericEnumMapByKey(Class<E> enumClass, SFunction<E, K> keyGetter) {
+        Map<Object, Enum<?>> map = enumMapByKey(enumClass, keyGetter);
+        Map<K, E> genericMap = new ConcurrentHashMap<>(map.size());
+        map.forEach((k, v) -> genericMap.put((K) k, (E) v));
+        return genericMap;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <E extends Enum<E>, K, V> Map<K, V> genericEnumMapByKey(Class<E> enumClass, SFunction<E, K> keyGetter,
+                                                                          SFunction<E, V> valueGetter) {
+        if (Objects.isNull(valueGetter)) {
+            return Map.of();
+        }
+        Map<Object, Enum<?>> map = enumMapByKey(enumClass, keyGetter);
+        Map<K, V> genericMap = new ConcurrentHashMap<>(map.size());
+        map.forEach((k, v) -> genericMap.put((K) k, valueGetter.apply((E) v)));
+        return genericMap;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <E extends Enum<E>, K> E getEnum(Class<E> enumClass,
+                                                   SFunction<E, K> keyGetter,
+                                                   K k) {
+        if (Objects.isNull(k)) {
+            return null;
+        }
+        return (E) enumMapByKey(enumClass, keyGetter).get(k);
     }
 
     public static <E extends Enum<E>, K, V> V getValue(Class<E> enumClass,
                                                        SFunction<E, K> keyGetter,
                                                        K k,
                                                        SFunction<E, V> valueGetter) {
+        if (Objects.isNull(valueGetter)) {
+            return null;
+        }
         E e = getEnum(enumClass, keyGetter, k);
         if (Objects.nonNull(e)) {
             return valueGetter.apply(e);
